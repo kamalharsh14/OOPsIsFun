1a554c90d6aa60279ef0209a1e2d6a57d7a4d30c
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-03-26 21:09:47 +0530

    Classes & Objects

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -5,14 +5,14 @@ public class ClassesAndObjects {
     public static void main(String[] args) {
         
         //Creating 'Person' class object 'p1'
-        Person p1 = new Person();
+        Person1 p1 = new Person1();
         p1.age = 22;
         p1.name = "Harsh";
         System.out.println("Name = "+p1.name);
         System.out.println("Age = "+p1.age);
 
         //Creating 'Person' class object 'p12'
-        Person p2 = new Person();
+        Person1 p2 = new Person1();
         p2.age = 22;
         p2.name = "Kamal";
         System.out.println("\nName = "+p2.name);
@@ -21,7 +21,7 @@ public class ClassesAndObjects {
 }
 
 // This is another Class
-class Person{
+class Person1{
 
     String name;
     int age;
