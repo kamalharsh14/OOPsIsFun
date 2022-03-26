e9b5a0ab56dc05186f9d7b21827e4d9a205d6c33
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-03-26 22:41:39 +0530

    Interface

---
+++ src/Interface.java
@@ -0,0 +1,16 @@
+public class Interface implements bike{
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start() {
+         
+    }
+}
+
+interface bike{
+  
+    void start();
+
+}
